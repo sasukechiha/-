62495775d64e4418b32a383ca3810225a5dd60ef
MichaelSensei <165430298+MichaelSensei@users.noreply.github.com>
2024-04-08 12:32:01 +0000

    Update set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -18,9 +18,9 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["100065927401614"];
+    const permission = ["100083695043563"];
   if (!permission.includes(event.senderID)) {
-    api.sendMessage("Gagne de l'argent comme les autres au lieu de tricher😴 \n\nSale fils de pute🖕 je n'accepte que merdi.", event.threadID, event.messageID);
+    api.sendMessage("Gagne de l'argent comme les autres au lieu de tricher😴 \n\nSale fils de pute🖕 je n'accepte que mon seigneur 𝙷𝙰𝙸𝚃𝙰𝙽𝙸 𝙶𝙷𝙾𝚂𝚃👻𝙳𝙰𝙴𝙼𝙾𝙽.", event.threadID, event.messageID);
     return;
   }
     const query = args[0];
