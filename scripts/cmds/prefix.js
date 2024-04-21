049120c4f03d1555d88b134dba742710143ae0d5
HTN225BOT <161234083+HTN225BOT@users.noreply.github.com>
2024-04-21 16:14:59 +0000

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "🌐 System prefix: %1\n🛸 Your box chat prefix: %2"
+			myPrefix: "༐⸙ 𝐒𝐲𝐬𝐭𝐞𝐦 𝐩𝐫𝐞𝐟𝐢𝐱: %1\n༐⸙ 𝐘𝐨𝐮𝐫 𝐛𝐨𝐱 𝐜𝐡𝐚𝐭 𝐩𝐫𝐞𝐟𝐢𝐱: %2 ┈┈┈┈┈┈┈┈┈┈⚋⚋┈┈┈┈┈┈┈┈┈┈┈࿂...𝗛𝗔𝗜𝗧𝗔𝗡𝗜...࿂┈┈┈┈"
 		}
 	},
 
