f7dcbe07c12aa553f70691eb45e5adb4de98e785
HTN225BOT <161234083+HTN225BOT@users.noreply.github.com>
2024-05-05 22:28:42 +0000

    Update ask.js

--- scripts/cmds/ask.js
+++ scripts/cmds/ask.js
@@ -1,17 +1,38 @@
 const axios = require('axios');
 
+const fonts = {
+
+    mathsans: {
+
+        a: "𝘢", b: "𝘣", c: "𝘤", d: "𝘥", e: "𝘦", f: "𝘧", g: "𝘨", h: "𝘩", i: "𝘪",
+
+        j: "𝘫", k: "𝘬", l: "𝘭", m: "𝘮", n: "𝘯", o: "𝘰", p: "𝘱", q: "𝘲", r: "𝘳",
+
+        s: "𝘴", t: "𝘵", u: "𝘶", v: "𝘷", w: "𝘸", x: "𝘹", y: "𝘺", z: "𝘻",
+
+        A: "𝘈", B: "𝘉", C: "𝘊", D: "𝘋", E: "𝘌", F: "𝘍", G: "𝘎", H: "𝘏", I: "𝘐",
+
+        J: "𝘑", K: "𝘒", L: "𝘓", M: "𝘔", N: "𝘕", O: "𝘖", P: "𝘗", Q: "𝘘", R: "𝘙",
+
+        S: "𝘚", T: "𝘛", U: "𝘜", V: "𝘝", W: "𝘞", X: "𝘟", Y: "𝘠", Z: "𝘡",
+    }
+};
+
 const Prefixes = [
-  'ask', 
+  '667',
   'ai',
-  'ghost' 
-];
+  'ghost',
+  'ask',
+  'Haitani', 
+   'Shisui',
+    'Street',];
 
 module.exports = {
   config: {
     name: "ask",
     version: 1.0,
-    author: "Naruto",
-    longDescription: "AI", 
+    author: "OtinXSandip | Aesther",
+    longDescription: "AI",
     category: "ai",
     guide: {
       en: "{p} questions",
@@ -20,34 +41,37 @@ module.exports = {
   onStart: async function () {},
   onChat: async function ({ api, event, args, message }) {
     try {
-      
+
       const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
       if (!prefix) {
         return; // Invalid prefix, ignore the command
       }
       const prompt = event.body.substring(prefix.length).trim();
-   if (!prompt) {
-        await message.reply(" ༐⸙.. 𝗚𝗛𝗢𝗦𝗧..✨👻 ╼╴╼╴╼╴╼╴╼╴╼╴╼╴╼╴         🕊𝚈𝙾 𝙵𝚁𝙰𝙽𝙶𝙸𝙽 𝙹𝙴 𝚂𝚄𝙸𝚂 𝚃𝙾𝙽 𝙰𝚂𝚂𝙸𝚂𝚃𝙰𝙽𝚃🤖 𝚅𝙸𝚁𝚃𝚄𝙴𝙻..🫶... 𝙿𝙾𝚂𝙴 𝙼𝙾𝙸 𝚃𝙰 𝚀𝚄𝙴𝚂𝚃𝙸𝙾𝙽 ༅    ╼╴╼╴╼╴╼╴╼╴╼╴╼╴╼╴               ༈ ࿂┉༐⸙ 𝗛𝗔𝗜𝗧𝗔𝗡𝗜.....❍♱")
+      if (!prompt) {
+        await message.reply("༐⸙ 𝗚𝗛𝗢𝗦𝗧✨👻 ╼╴╼╴╼╴╼╴╼╴╼╴╼╴╼╴         🕊𝚈𝙾 𝙵𝚁𝙰𝙽𝙶𝙸𝙽 𝙹𝙴 𝚂𝚄𝙸𝚂 𝚃𝙾𝙽 𝙰𝚂𝚂𝙸𝚂𝚃𝙰𝙽𝚃🤖 𝚅𝙸𝚁𝚃𝚄𝙴𝙻..🫶... 𝙿𝙾𝚂𝙴 𝙼𝙾𝙸 𝚃𝙰 𝚀𝚄𝙴𝚂𝚃𝙸𝙾𝙽 ༅    ╼╴╼╴╼╴╼╴╼╴╼╴╼╴╼╴               ༈ ࿂┉༐⸙ 𝗛𝗔𝗜𝗧𝗔𝗡𝗜.....❍♱ ");
         return;
       }
-
-
+      const senderID = event.senderID;
+      const senderInfo = await api.getUserInfo([senderID]);
+      const senderName = senderInfo[senderID].name;
       const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
-      const answer = response.data.answer;
+      const answer = `➪❘❘❘『🅖🅗🅞🅢🅣 』❘❘❘  ──────────────
 
- 
-    await message.reply({ body: `➬.༐☄️👻 𝗚𝗛𝗢𝗦𝗧  ༐🪶🩷....✰
-▭▭▭▭▭▭▭▭▭▭▭▭
+${response.data.answer}
 
+ ━━━━━━━━━━━━━━━━ 
+|✰|👻`;
 
-${answer}
-
-
-▢▢▢✞྿..𝗛𝗔𝗜𝗧𝗔𝗡𝗜..྿✞▢▢▢`,
-});
-
-   } catch (error) {
-      console.error("Error:", error.message);
-    }uw
-  }
+      //apply const font to each letter in the answer
+      let formattedAnswer = "";
+      for (let letter of answer) {
+        formattedAnswer += letter in fonts.mathsans ? fonts.mathsans[letter] : letter;
       }
+
+      await message.reply(formattedAnswer);
+
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  }
+};
